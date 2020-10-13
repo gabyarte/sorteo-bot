f5c2164cf65c19a76a323db1c02f0c5018c617b8
Gabriela Arguelles <gabyarte2@gmail.com>
2020-10-13 16:51:30 -0400

    Remove django project

--- src/sorteo/sorteo_api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from . import models
-
-admin.site.register(models.User)
-admin.site.register(models.Number)
-admin.site.register(models.Raffle)
--- src/sorteo/sorteo_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SorteoApiConfig(AppConfig):
-    name = 'sorteo_api'
