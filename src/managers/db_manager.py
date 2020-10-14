858d9f40458425aee484573b157b04ae0ad38bc6
Gabriela Arguelles <gabyarte2@gmail.com>
2020-10-14 12:25:09 -0400

    Add database manager metaclass

--- src/managers/db_manager.py
+++ src/managers/db_manager.py
@@ -2,13 +2,24 @@ from pymongo import MongoClient
 
 import settings
 
-class DatabaseManager:
-    _client = MongoClient(settings.DB_URI)
-    _db = _client.sorteo
+class MetaDatabaseManager(type):
+    @property
+    def db(cls):
+        return cls.get_db()
+
+class DatabaseManager(metaclass=type):
+    _client, _db = None, None
+
+    @classmethod
+    def get_db(cls):
+        if cls._client is None:
+            cls._client = MongoClient(settings.DB_URI)
+            cls._db = cls._client.sorteo
+        return cls._db
 
     @classmethod
     def _insert(cls, table, data):
-        collection = cls._db[table]
+        collection = cls.db[table]
         return collection.insert_many(data) if isinstance(data, list) else collection.insert_one(data)
 
     @classmethod
