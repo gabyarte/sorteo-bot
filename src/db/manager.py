35fbff08065720f38ce5a93303e55b2efa7514ab
Gabriela Arguelles <gabyarte2@gmail.com>
2020-10-20 17:46:44 -0400

    Refactor database manager

--- src/db/manager.py
+++ src/db/manager.py
@@ -28,44 +28,33 @@ class DatabaseManager(metaclass=MetaDatabaseManager):
     def collection(cls, *slots):
         def init(self, **fields):
             for key, value in fields.items():
-                setattr(self, key, value)
+                if key in slots:
+                    setattr(self, key, value)
 
         def wrapper(model):
             model.__init__ = init
-            model.documents = Documents(cls.db[model.__name__.lower()], slots, model)
+            model.documents = Documents(cls.db[model.__name__.lower()], model)
             return model
         return wrapper
 
 
 class Documents:
-    def __init__(self, collection, slots, model):
+    def __init__(self, collection, model):
         self._collection = collection
-        self._slots = slots
         self._model = model
 
-    def _to_model(self, data):
-        instance = self._model()
-        for key, value in data.items():
-            if key in self._slots:
-                setattr(instance, key, value)
-
-    def _to_dict(self, model):
-        return {slot: getattr(model, slot) for slot in self._slots}
-
-    def is_valid(self, data):
-        return set(data.keys()) - set(self._slots) == set()
-
     def insert(self, data):
+        logging.info(f'model - {self._model}')
         result = self._collection.insert_one(data)
         logging.info(f'insert - {result}\ninserted_id - {result.inserted_id}')
-        return self._to_model(data) if result.inserted_id else None
+        return self._model(**data) if result.inserted_id else None
 
     def delete(self, document):
         delete_query = {'_id': document} if isinstance(document, int) else document
         return self._collection.delete_one(delete_query) 
 
     def find(self, query):
-        return {self._to_model(document) for document in self._collection.find(query, self.fields)}
+        return {self._model(**document) for document in self._collection.find(query, self.fields)}
 
     def values_list(self, query, value):
         return [getattr(document, value) for document in self.find(query)]
