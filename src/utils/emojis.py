c5bb4089eab0f56f62fd5ab32788cf254ae54938
Gabriela Arguelles <gabyarte2@gmail.com>
2020-10-29 22:44:29 -0400

    Test emoji unicode pensive

--- src/utils/emojis.py
+++ src/utils/emojis.py
@@ -3,5 +3,6 @@ from emoji import emojize
 CROSS = u"\u274C"
 WINK = emojize(':wink:')
 RELAX = emojize(':relieved:')
-PENSIVE = emojize(':pensive:')
+# PENSIVE = emojize(':pensive:')
+PENSIVE = '\U0001F614'
 SMIRK = emojize(':smirk:')
