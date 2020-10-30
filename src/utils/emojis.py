bd18f0c94a7ba437235dd7ca6bf2f4e66a3473b7
Gabriela Arguelles <gabyarte2@gmail.com>
2020-10-29 22:50:31 -0400

    Add wink emoji

--- src/utils/emojis.py
+++ src/utils/emojis.py
@@ -1,7 +1,7 @@
 from emoji import emojize
 
 CROSS = u"\u274C"
-WINK = emojize(':wink:')
+WINK = emojize('\U0001F609')
 RELAX = emojize(':relieved:')
 # PENSIVE = emojize(':pensive:')
 PENSIVE = '\U0001F614'
