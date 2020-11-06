4d363cd5b09d0dfd3a773b2c7c43f93823711966
Gabriela Arguelles <gabyarte2@gmail.com>
2020-11-05 19:13:45 -0500

    Fix issue when unblocking

--- src/commands/list_block_users.py
+++ src/commands/list_block_users.py
@@ -18,7 +18,7 @@ def start(update, context):
             name = f'{chat.first_name} {chat.last_name}' if chat.first_name and chat.last_name else chat.username
             name = name if name else chat.first_name
 
-            list_blocked.append([InlineKeyboardButton(f'{name}', callback_data=f'unblock/{user_id}')])
+            list_blocked.append([InlineKeyboardButton(f'{name}', callback_data=f'unblock/{blocked_user.telegram_id}')])
 
         update.message.reply_text(f'Bloqueados\n\n{DANGER} Si selecciones un usuario bloqueado, lo puedes *DESBLOQUEAR* y podrá participar en los sorteos disponibles', parse_mode=ParseMode.MARKDOWN_V2, reply_markup=InlineKeyboardMarkup(list_blocked + [CANCEL_MARKUP]))
 
