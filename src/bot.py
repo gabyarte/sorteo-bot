d5c7dac1549b3f6c4883c3f0ea1f73a8daf8f9d3
Gabriela Arguelles <gabyarte2@gmail.com>
2020-11-05 19:02:26 -0500

    Add blocked_handler

--- src/bot.py
+++ src/bot.py
@@ -11,6 +11,7 @@ from src.commands.participate import participate_raffle_handler
 from src.commands.list_my_raffles import my_raffles_handler
 from src.commands.list_all_raffles import all_raffles_handler
 from src.commands.raffle_query_flow import raffle_callback_query_handler
+from src.commands.list_block_users import list_blocked_handler
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -26,6 +27,8 @@ class RaffleBot:
         dp.add_handler(raffle_callback_query_handler)
         dp.add_handler(my_raffles_handler)
         dp.add_handler(all_raffles_handler)
+        dp.add_handler(list_blocked_handler)
+
 
         updater.start_webhook(listen="0.0.0.0",
                               port=PORT,
